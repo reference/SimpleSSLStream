e1cd0a2473429874b7f9fd2b7c358f07baf18c53
x coder <xcoder@new-host-2.beijing.netspectrum.com>
2012-07-09 20:57:34 +0800

    Initial Commit

---
+++ SimpleSSLStream/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleSSLStream
+//
+//  Created by x coder on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
